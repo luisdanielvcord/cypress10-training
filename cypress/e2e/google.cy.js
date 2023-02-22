c59ce72c7ceba4aa710f6366cc44300da46826c4
luisdanielvcord <daniel.vasquezc@perficient.com>
2023-02-22 11:25:39 -0500

    first cypress test

---
+++ cypress/e2e/google.cy.js
@@ -0,0 +1,7 @@
+describe("This is my first cypress test", () => {
+    it("should have a title", () => {
+      cy.visit("https://www.google.com/");
+      cy.title().should("eq", "Google");
+    });
+  });
+  
