29b0e2533204ccc322d0e7a6df61e6b29febe0be
luisdanielvcord <83845890+luisdanielvcord@users.noreply.github.com>
2023-02-27 11:12:44 -0500

    Github Actions (#6)

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // modify config values examples
       // config.defaultCommandTimeout = 10000
