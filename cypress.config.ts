1637eeb0c10bfc5467f2b384756a95782ce15df7
luisdanielvcord <daniel.vasquezc@perficient.com>
2023-03-15 08:44:06 -0500

    Adding Reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,17 @@ export default defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      reporterEnabled: "mochawesome",
+      mochawesomeReporterOptions: {
+        reportDir: "cypress/reports/mocha",
+        quiet: true,
+        overwrite: false,
+        html: false,
+        json: true,
+      },
+    },
     video: false,
     screenshotOnRunFailure: false,
     chromeWebSecurity: false,
